58d84149ab65139203157362a2c1e23dc0747054
chandu970 <cchandu6136@gmail.com>
2023-03-30 23:18:35 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    topic=models.CharField(max_length=100,primary_key=True)
+class Webpage(models.Model):
+    topic=models.ForeignKey(Topic(), on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    url=models.URLField()
+    
+    def __str__(self):
+        return self.name
+    
+class AccessRecords(models.Model):
+    name=models.ForeignKey(Webpage, on_delete=models.CASCADE)
+    date=models.DateField()
